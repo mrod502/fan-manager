91d83d7f3a6d1d5b35819913666440141e7618d3
mrod502 <mrod22502@gmail.com>
2021-10-21 20:44:24 -0400

    works, maybe

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 
 #include "Performance/fanManager.hpp"
 #include "Performance/temperatureMonitor.hpp"
+#include <wiringPi.h>
+
+const int PWM_pin = 1;
+void setPWMWidth(int w){
+  pwmWrite(PWM_pin, i);
+};
+
 
 int main(){
 
 //  Performance::FanManager *mgr = new Performance::FanManager(0,1<<7,110,180);
-  Performance::FanManager *tm = new Performance::FanManager(0, 1024, 40, 70);
+  Performance::FanManager *fm = new Performance::FanManager(0, 1024, 40, 70);
 
-  tm->manage();
+  fm->manage(setPWMWidth);
 
   delete tm;
 
